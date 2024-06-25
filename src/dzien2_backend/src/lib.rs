0eed7e989259c20bea81740451b9c50406010fb5
TheGift20obj <djadunov@gmail.com>
2024-06-25 18:55:53 +0200

    added way to add wpisy

--- src/dzien2_backend/src/lib.rs
+++ src/dzien2_backend/src/lib.rs
@@ -1,4 +1,17 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static  WPISY: RefCell<Vec<String>> = RefCell::default();
+}
+
 #[ic_cdk::query]
 fn greet(name: String, surrname: i8) -> String {
     format!("Hello, {} {}!", name, surrname)
+}
+
+#[ic_cdk::update]
+fn dodaj_wpis(wpis: String) {
+    WPISY.with(|wpisy| {
+        wpisy.borrow_mut().push(wpis)
+    })
 }
